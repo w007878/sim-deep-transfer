cec43f41769273a222dc2f1e2423f503a49a16e7
snowyjone <w007878@hotmail.com>
2017-08-01 15:22:15 +0800

    build the main network model

--- model.py
+++ model.py
@@ -4,6 +4,60 @@ import cv2
 import os
 import load_data
 
-class model:
+def init_bais_variable(shape):
+    initial = tf.constant(-0.1, shape=shape)
+    return tf.Variable(initial)
+
+def init_weight_variable(shape):
+    initial = tf.random_normal(shape=shape, stddev=0.1)
+    return tf.Varialbe(initial)
+
+def conv2d(x, W):
+    return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
+
+def max_pool_2x2(x):
+    return tf.nn.max_pool(x, kisize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+          
+class Main_model:
     def __init__(self):
+        self.input_data = tf.placeholder(tf.float32, [None, 784])
+        self.input_image = tf.reshape(self.input_data, [-1, 28, 28, 1])
         
+        self.W_conv1 = init_weight_variable([3, 3, 1, 32])
+        self.b_conv1 = init_bais_variable([32])
+        self.h_conv1 = tf.nn.relu(conv2d(self.input_image, self.W_conv1) + self.b_conv1) 
+        self.h_pool1 = max_pool_2x2(h_conv1)
+        
+        self.W_conv2 = init_weight_variable([3, 3, 32, 64])
+        self.b_conv2 = init_bais_variable([64])
+        self.h_conv2 = tf.nn.relu(conv2d(self.h_pool1, self.W_conv2) + self.b_conv2) 
+
+        self.W_conv3 = init_weight_variable([3, 3, 32, 64])
+        self.b_conv3 = init_bais_variable([64])
+        self.h_conv3 = tf.nn.relu(conv2d(self.h_conv2, self.W_conv3) + self.b_conv3) 
+        self.h_pool3 = max_pool_2x2(h_conv3)
+        
+        self.W_conv4 = init_weight_variable([3, 3, 32, 64])
+        self.b_conv4 = init_bais_variable([64])
+        self.h_conv4 = tf.nn.relu(conv2d(self.h_pool3, self.W_conv4) + self.b_conv4) 
+
+        self.W_conv5 = init_weight_variable([3, 3, 32, 128])
+        self.b_conv5 = init_bais_variable([128])
+        self.h_conv5 = tf.nn.relu(conv2d(self.h_conv4, self.W_conv5) + self.b_conv5) 
+        self.h_conv5_flat = tf.reshape(self.h_conv5, [-1, 7 * 7 * 128])
+        
+        self.W_fc6 = init_weight_variable([7 * 7 * 128, 512])
+        self.b_fc6 = init_bais_variable([512])
+        self.h_fc6 = tf.nn.relu(tf.matmul(self.h_conv5_flat, self.W_fc6) + self.b_fc6)
+        
+        self.W_fc7 = init_weight_variable([512, 1024])
+        self.b_fc7 = init_bias_variable([1024])
+        self.h_fc7 = tf.nn.relu(tf.matmul(self.h_fc6, self.W_fc7) + self.b_fc7)
+        
+        self.W_fc8 = init_weight_variable([1024, 10])
+        self.b_fc8 = init_bias_variable([10])
+        self.h_fc8 = tf.nn.relu(tf.matmul(self.h_fc7, self.W_fc8) + self.b_fc8)
+        
+        self.W_fcD = init_weight_variable([1024, 2])
+        self.b_fcD = init_bias_variable([2])
+        self.h_fcD = tf.nn.sigmoid(tf.matmul(self.h_fc7, self.W_fcD) + self.b_fcD)
