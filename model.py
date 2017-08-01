d78e135359425af486f46db83157c1dafb470802
snowyjone <w007878@hotmail.com>
2017-08-01 22:46:33 +0800

    modified

--- model.py
+++ model.py
@@ -19,7 +19,7 @@ def max_pool_2x2(x):
     return tf.nn.max_pool(x, kisize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
           
 class Main_model:
-    def __init__(self):
+    def __init__(self, keep_rate=1.0):
         self.input_data = tf.placeholder(tf.float32, [None, 784])
         self.input_image = tf.reshape(self.input_data, [-1, 28, 28, 1])
         
@@ -49,15 +49,30 @@ class Main_model:
         self.W_fc6 = init_weight_variable([7 * 7 * 128, 512])
         self.b_fc6 = init_bais_variable([512])
         self.h_fc6 = tf.nn.relu(tf.matmul(self.h_conv5_flat, self.W_fc6) + self.b_fc6)
+        self.h_fc6_drop = tf.nn.dropout(self.h_fc6, keep_rate)
         
         self.W_fc7 = init_weight_variable([512, 1024])
         self.b_fc7 = init_bias_variable([1024])
-        self.h_fc7 = tf.nn.relu(tf.matmul(self.h_fc6, self.W_fc7) + self.b_fc7)
+        self.h_fc7 = tf.nn.relu(tf.matmul(self.h_fc6_drop, self.W_fc7) + self.b_fc7)
+        self.h_fc7_drop = tf.nn.dropout(self.h_fc7, keep_rate)
         
         self.W_fc8 = init_weight_variable([1024, 10])
         self.b_fc8 = init_bias_variable([10])
-        self.h_fc8 = tf.nn.relu(tf.matmul(self.h_fc7, self.W_fc8) + self.b_fc8)
+        self.h_fc8 = tf.nn.relu(tf.matmul(self.h_fc7_drop, self.W_fc8) + self.b_fc8)
         
         self.W_fcD = init_weight_variable([1024, 2])
         self.b_fcD = init_bias_variable([2])
         self.h_fcD = tf.nn.sigmoid(tf.matmul(self.h_fc7, self.W_fcD) + self.b_fcD)
+    
+    def classifier_loss(self, label, logit):
+        return tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=label, logits=logit))
+        
+    def soft_loss(self, label, temperature):
+        l = tf.Variable(float32, shape=[10, 10])
+        
+        
+    def main_loss(self, label, domain_label, alpha=0.01, beta=0.1, temperature=10.0):
+        cls_loss = self.classifier_loss(label, self.fc8)
+        confusion_loss = self.classifier_loss(tf.constant(0,5, shape=[2]), self.fcD)
+        soft_loss = 
+        return cls_loss + alpha * confusion_loss + beta * soft_loss
